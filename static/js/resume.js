d44a5934170766e909391c22040562ac5dfbfa8f
Cyb3r-Jak3 <git@Cyberjake.xyz>
2023-02-01 19:03:27 -0500

    Update with 2023 information

--- static/js/resume.js
+++ static/js/resume.js
@@ -1,22 +1,31 @@
+const resume_api_endpoint = "https://api.cyberjake.xyz/encrypted_resume"
+
 $("#resume_form").submit(function downloadFile(evt) {
 	evt.preventDefault();
 	var formData = new FormData($(this)[0]);
-	var req = new XMLHttpRequest();
-	req.setRequestHeader("X-API-LOCATION", window.location.hostname)
-	req.open("POST", "https://api.cyberjake.xyz/encrypted_resume", true);
-	req.responseType = "blob";
-	req.onload = function (event) {
-		var blob = req.response;
-		try {
-			var fileName = req.getResponseHeader('content-disposition').split("filename=")[1];
-		} catch (err) {
-			var fileName = "jwhite_signed_resume.pdf.gpg"
+	fetch(
+		resume_api_endpoint,
+		{
+			method: "POST",
+			headers: {
+				"User-Agent": "Portfolio",
+				"X-API-LOCATION": window.location.hostname
+			},
+			body: formData
 		}
-		var link=document.createElement('a');
-		link.href=window.URL.createObjectURL(blob);
-		link.download=fileName;
-		link.click();
-	};
-
-	req.send(formData);
+	).then( response => {
+		response.blob().then(blob => {
+			try {
+				var fileName = response.headers.get('content-disposition').split("filename=")[1];
+			} catch (err) {
+				var fileName = "jwhite_signed_resume.pdf.gpg"
+			}
+			var link=document.createElement('a');
+			link.href=window.URL.createObjectURL(blob);
+			link.download=fileName;
+			link.click();
+		}
+		)
+	}
+	)
 })
